494e9dfc5cc569beb02624c98b2b6ce80d6b86fa
Vladimir <vladimirdd96@gmail.com>
2020-09-14 11:44:30 +0300

    dom

---
+++ DOM theory ex/article list/main.ts
@@ -0,0 +1,23 @@
+const articlesBox = document.querySelector("#articles");
+
+articlesBox.className = 'articlesBox';
+
+function createArticle() {
+    const article = document.createElement("article");
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    const input = document.querySelector("#createTitle");
+    const text = document.querySelector("#createContent");
+
+    if (input === null || input.value === "" || text === null) {
+        throw new Error("Something went wrong..")
+    }
+
+
+    article.appendChild(h3);
+    article.appendChild(p);
+    articlesBox.appendChild(article);
+
+    h3.innerHTML = input.value;
+    p.innerHTML = text.value;
+}
