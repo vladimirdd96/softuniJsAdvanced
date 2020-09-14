04d4218b24be4988a0fa084aec9e47c1ff4e2979
Vladimir <vladimirdd96@gmail.com>
2020-09-14 13:52:31 +0300

    dom manip lab ex 1

--- DOM Manipulation/DOM Manipulations theory ex/solution.js
+++ DOM Manipulation/DOM Manipulations theory ex/solution.js
@@ -1,7 +1,23 @@
+function createElement(type, content) {
+    const e = document.createElement(type)
+    e.innerHTML = content
+    return e
+}
+
+function callBackF() {
+    const list = document.getElementById("items")
+    const input = document.getElementById("newItemText")
+
+    if (list === null || input.value === "") {
+        throw new Error("Please enter smth")
+    }
+
+    list.appendChild(createElement('li', input.value))
+}
+
 function main() {
     const butt = document.getElementById("addCTA")
-    const list = document.getElementById("newItemText")
-    
+    butt.addEventListener('click', callBackF)
 }
 
 const onPageLoad = document.addEventListener('DOMContentLoaded', main())
