e2f1a9ee31a3420193e9405324c38ee50328a7c4
Vladimir <vladimirdd96@gmail.com>
2020-09-14 12:04:19 +0300

    refactor the folders

---
+++ DOM Manipulation/DOM Manipulations theory ex/solution.js
@@ -0,0 +1,7 @@
+function main() {
+    const butt = document.getElementById("addCTA")
+    const list = document.getElementById("newItemText")
+    
+}
+
+const onPageLoad = document.addEventListener('DOMContentLoaded', main())
