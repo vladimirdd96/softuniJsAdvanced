e0e33cc6e98f9909b9a007f1b199336761c3cc27
Vladimir <vladimirdd96@gmail.com>
2020-09-21 12:02:39 +0300

    classes lab done

---
+++ Classes/classes theory ex/Cards/solution.ts
@@ -0,0 +1,57 @@
+let result = (function () {
+    let Suits = {
+        SPADES: "♠",
+        HEARTS: "♥",
+        DIAMONDS: "♦",
+        CLUBS: "♣"
+    }
+    let face = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    class Card {
+        private _face
+        private _suit
+        constructor(face: string, suit: string) {
+            this.face = face
+            this.suit = suit
+        }
+
+        get face() {
+            return this._face
+        }
+        set face(value) {
+            if (!face.includes(value)) {
+                throw new Error('invalid face value')
+            }
+
+            this._face = value
+        }
+
+        get suit() {
+            return this._suit
+        }
+
+        set suit(value) {
+            if (!Object.keys(Suits).map(x => Suits[x]).includes(value)) {
+                throw new Error('invalid suit value')
+            }
+
+            this._suit = value
+        }
+    }
+
+
+
+    return {
+        Suits: Suits,
+        Card: Card
+    }
+}())
+
+let Card = result.Card
+let Suits = result.Suits
+
+let card = new Card('Q', Suits.CLUBS)
+card.face = 'A'
+card.suit = Suits.DIAMONDS
+console.log(card);
+// let card2 = new Card('1', Suits.DIAMONDS)
